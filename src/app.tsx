7e5c7211356101b1f357cca9108b35a2cdc57cb2
keflag <xuemingke2009@163.com>
2026-03-08 11:25:01 +0800

    fix(app): 修改导出方式为默认导出

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 
-export function Root(props: { children: React.ReactNode }) {
+const App = (props: { children: React.ReactNode }) => {
   return (
     <AuthProvider>
       {props.children}
     </AuthProvider>
   );
-}
+};
 
-export default Root;
+export default App;
